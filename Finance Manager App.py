7b4ae454605efa0e8b6b06352ab9015af01c669e
cshevick <107512545+cshevick@users.noreply.github.com>
2024-06-28 19:51:13 -0400

    Add files via upload

---
+++ Finance Manager App.py
@@ -0,0 +1,12 @@
+#import statements for other files
+import PySimpleGUI as sg
+import loginScreen
+import menuScreen
+import stockScreen
+import billManageScreen
+import incomeStatementScreen
+
+
+
+#starts the app
+loginScreen.login()
