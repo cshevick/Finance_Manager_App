18015e42e8df526acfad2939cbdc92a523125806
cshevick <107512545+cshevick@users.noreply.github.com>
2024-07-04 18:01:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 #import statements for other files
-import PySimpleGUI as sg
-import loginScreen
-import menuScreen
-import stockScreen
-import billManageScreen
-import incomeStatementScreen
-import learnScreen
+import databaseConnect
 
 
-#starts the app
-loginScreen.login()
+databaseConnect.start_screen()
